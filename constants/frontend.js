4514b5decabbeaeac9c928729566202a8f0eaffd
razker <41743968+razker@users.noreply.github.com>
2021-03-16 10:15:02 +0200

    Update frontend.js

--- constants/frontend.js
+++ constants/frontend.js
@@ -1,11 +1,9 @@
 const FRONTEND_DEV_URLS = [ 'http://localhost:3000', 'http://localhost:3001' ];
 
 const FRONTEND_PROD_URLS = [
-  'https://www.yourdomain.com',
-  'https://yourdomain.com'
+  'https://simple-menu-web.herokuapp.com/',
 ];
 
 module.exports = process.env.NODE_ENV === 'production'
     ? FRONTEND_PROD_URLS
     : FRONTEND_DEV_URLS;
-    
